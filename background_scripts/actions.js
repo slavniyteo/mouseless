1995eaton/chromium-vim

commit 0e249e0b1b96f43ee06803bc9c23e01114374990

make buffers in current window have priority

background_scripts/actions.js
@@ -697,15 +697,26 @@ Actions = (function() {
 
   _.getBuffers = function(o) {
     chrome.tabs.query({}, function(tabs) {
+      var otherWindows = [];
+      tabs = tabs.filter(function(e) {
+        if (e.windowId === o.sender.tab.windowId)
+          return true;
+        otherWindows.push(e);
+        return false;
+      });
+      tabs = tabs.concat(otherWindows);
+
+      var buffers = tabs.map(function(e, i) {
+        var title = e.title;
+        if (settings.showtabindices) {
+          title = title.replace(new RegExp('^' + (e.index + 1) + ' '), '');
+        }
+        return [(i + 1) + ': ' + title, e.url];
+      });
+
       o.callback({
         type: 'buffers',
-        buffers: tabs.map(function(e, i) {
-          var title = e.title;
-          if (settings.showtabindices) {
-            title = title.replace(new RegExp('^' + (e.index + 1) + ' '), '');
-          }
-          return [(i + 1) + ': ' + title, e.url, e.id];
-        })
+        buffers: buffers
       });
     });
   };

slavniyteo/mouseless

commit e21999740d67c907128b9541636327cfbd288fbf

fixed #107 by reverting regression from 0e249e0b1b96f43ee06803bc9c23e01114374990 Revert "make buffers in current window have priority" This reverts commit 0e249e0b1b96f43ee06803bc9c23e01114374990.

background_scripts/actions.js
@@ -878,26 +878,15 @@ Actions = (function() {
 
   _.getBuffers = function(o) {
     chrome.tabs.query({}, function(tabs) {
-      var otherWindows = [];
-      tabs = tabs.filter(function(e) {
-        if (e.windowId === o.sender.tab.windowId)
-          return true;
-        otherWindows.push(e);
-        return false;
-      });
-      tabs = tabs.concat(otherWindows);
-
-      var buffers = tabs.map(function(e, i) {
-        var title = e.title;
-        if (settings.showtabindices) {
-          title = title.replace(new RegExp('^' + (e.index + 1) + ' '), '');
-        }
-        return [(i + 1) + ': ' + title, e.url];
-      });
-
       o.callback({
         type: 'buffers',
-        buffers: buffers
+        buffers: tabs.map(function(e, i) {
+          var title = e.title;
+          if (settings.showtabindices) {
+            title = title.replace(new RegExp('^' + (e.index + 1) + ' '), '');
+          }
+          return [(i + 1) + ': ' + title, e.url, e.id];
+        })
       });
     });
   };